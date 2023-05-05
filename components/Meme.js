1e27854ddc6f0fa4365739a4d7b625ebb018e4b8
Lilian Lee <lilianlee0213@gmail.com>
2023-05-05 13:58:31 -0400

    get  random meme

--- components/Meme.js
+++ components/Meme.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import memesData from '../memesData';
 
 export default function Meme() {
+	function getMemeImage() {
+		const memesArray = memesData.data.memes;
+		const randomNumbner = Math.floor(Math.random() * memesArray.length);
+		const url = memesArray[randomNumbner].url;
+		console.log(url);
+	}
+	const onSubmit = (e) => {
+		e.preventDefault();
+	};
 	return (
 		<main>
-			<form className="form">
+			<form className="form" onSubmit={onSubmit}>
 				<input type="text" placeholder="Top text" className="form--input" />
 				<input type="text" placeholder="Bottom text" className="form--input" />
-				<button className="form--button">Get a new meme image</button>
+				<button className="form--button" onClick={getMemeImage}>
+					Get a new meme image
+				</button>
 			</form>
 		</main>
 	);
