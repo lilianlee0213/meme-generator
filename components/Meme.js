01a3f17eb93afefa30bf70509bfdff53629a7ba1
Lilian Lee <lilianlee0213@gmail.com>
2023-05-08 12:41:42 -0400

    addText to image

--- components/Meme.js
+++ components/Meme.js
@@ -5,7 +5,7 @@ export default function Meme() {
 	const [meme, setMeme] = useState({
 		topText: '',
 		bottomText: '',
-		randomImage: '',
+		randomImage: 'https://i.imgflip.com/30b1gx.jpg',
 	});
 	const [allMemeImages, setAllMemeImages] = useState(memesData);
 
@@ -18,20 +18,43 @@ export default function Meme() {
 			randomImage: url,
 		}));
 	}
+	const handleChange = (event) => {
+		const {name, value} = event.target;
+		setMeme((prevMeme) => ({
+			...prevMeme,
+			[name]: value,
+		}));
+	};
 	const onSubmit = (e) => {
 		e.preventDefault();
 	};
 	return (
 		<main>
 			<form className="form" onSubmit={onSubmit}>
-				<input type="text" placeholder="Top text" className="form--input" />
-				<input type="text" placeholder="Bottom text" className="form--input" />
+				<input
+					type="text"
+					placeholder="Top text"
+					className="form--input"
+					name="topText"
+					value={meme.topText}
+					onChange={handleChange}
+				/>
+				<input
+					type="text"
+					placeholder="Bottom text"
+					className="form--input"
+					name="bottomText"
+					value={meme.bottomText}
+					onChange={handleChange}
+				/>
 				<button className="form--button" onClick={getMemeImage}>
 					Get a new meme image
 				</button>
 			</form>
-			<div>
-				<img src={meme.randomImage} alt="" />
+			<div className="meme">
+				<img src={meme.randomImage} className="meme-image" />
+				<h2 className="meme--text top">{meme.topText}</h2>
+				<h2 className="meme--text bottom">{meme.bottomText}</h2>
 			</div>
 		</main>
 	);
